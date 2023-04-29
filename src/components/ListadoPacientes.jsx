463328dfdc6ee73da1eb2ba8a2df1830ec05aee9
Facundo <facusaavedrx@gmail.com>
2023-04-29 10:49:27 -0300

    Se agrego uuid al proyecto y funcionalidad que muestra pacientes en el listado

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,6 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
 import Paciente from './Paciente'
 
-function ListadoPacientes () {
+function ListadoPacientes ({ pacientes }) {
   return (
     <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
       <h2 className='font-black text-3xl text-center'>Pacientes</h2>
@@ -8,7 +9,11 @@ function ListadoPacientes () {
         Administrar <span className='text-indigo-600 font-bold'> pacientes</span> y <span className='text-indigo-600 font-bold'>citas </span>
       </p>
 
-      <Paciente />
+      {pacientes.map(paciente => {
+        return (
+          <Paciente pacientes={paciente} key={uuidv4()} />
+        )
+      })}
     </div>
   )
 }
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,26 +1,27 @@
 import { useState, useEffect } from 'react'
 
-function Paciente () {
+function Paciente ({ pacientes }) {
+  const { mascota, propietario, email, fecha, sintomas } = pacientes
   return (
     <div className='mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
       <p className='font-bold mb-3 text-gray-700'>
-        Mascota: <span className='font-normal normal-case'>Lucy</span>
+        Mascota: <span className='font-normal normal-case'> {mascota} </span>
       </p>
 
       <p className='font-bold mb-3 text-gray-700'>
-        Dueño / Dueña: <span className='font-normal normal-case'>Facundo Saavedra</span>
+        Dueño / Dueña: <span className='font-normal normal-case'> {propietario} </span>
       </p>
 
       <p className='font-bold mb-3 text-gray-700'>
-        Correo electronico: <span className='font-normal normal-case'>facusaavedrx@gmail.com</span>
+        Correo electronico: <span className='font-normal normal-case'> {email} </span>
       </p>
 
       <p className='font-bold mb-3 text-gray-700'>
-        Fecha de alta: <span className='font-normal normal-case'>07/07/1998</span>
+        Fecha de alta: <span className='font-normal normal-case'> {fecha} </span>
       </p>
 
       <p className='font-bold mb-3 text-gray-700'>
-        Sintomas: <span className='font-normal normal-case'>Dolor de cabeza</span>
+        Sintomas: <span className='font-normal normal-case'> {sintomas} </span>
       </p>
     </div>
   )
