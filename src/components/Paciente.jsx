71f7a2d7c7169adaf46a6455667a26f41a495933
Facundo Saavedra <facusaavedrx@gmail.com>
2023-05-11 15:25:58 -0300

    Se agrego boton Editar y Eliminar, ambos sin funcionalidad

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -23,6 +23,24 @@ function Paciente ({ pacientes }) {
       <p className='font-bold mb-3 text-gray-700'>
         Sintomas: <span className='font-normal normal-case'> {sintomas} </span>
       </p>
+
+      <div
+        className='flex justify-between'
+      >
+        <button
+          type='button'
+          className='py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg'
+        >
+          Editar
+        </button>
+
+        <button
+          type='button'
+          className='py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg'
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   )
 }
